524e0fff9d0f8b9bc263ffaa13fc39d9d2da59d3
zleyyij <zleyyij@noreply.users.github.com>
2023-04-30 17:12:03 -0600

    sync

---
+++ core/util.ts
@@ -0,0 +1,9 @@
+/*
+This file re-exports core utilities needed for developing modules, consolidating imports/exports, it's expected
+that it'll be used kind of like `import * as util from ./core/util`
+*/
+export * from "./config.js";
+export * from "./embed.js";
+export * from "./logger.js";
+export * from "./modules.js";
+export * from "./main.js";
