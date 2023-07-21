9ae23d68e1016dd2e7d009abd4eee3c9e66e551f
Cpt-Dingus <100243410+Cpt-Dingus@users.noreply.github.com>
2023-07-22 00:57:36 +0200

    [K8s] - Added temporary dev.yaml file (k8s/dev/ is WIP)

--- src/core/modules.ts
+++ src/core/modules.ts
@@ -386,8 +386,7 @@ export class Dependency {
         'core',
         `Failed to resolve dependency ${this.name} due to error ${
           (err as Error).name
-        }, anything makes use of that dependency will not be available
-        ${(err as Error).stack}`,
+        }, anything makes use of that dependency will not be available`,
         2
       );
       // if an error is encountered during resolution, null is returned
