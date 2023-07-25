81e26b253224e6e578f4bbd6c48c81fa74633572
Cpt-Dingus <100243410+Cpt-Dingus@users.noreply.github.com>
2023-07-25 18:55:36 +0200

    [Tests]: Commented out import to stop make format from being pissy

--- src/test/main.test.ts
+++ src/test/main.test.ts
@@ -1,3 +1,5 @@
-import {describe, it} from 'node:test';
+//import {describe, it} from 'node:test';
+// commented out to prevent npm fix from complaining
+
 // dynamic import statements are used to avoid side affects
 // this is all broken
