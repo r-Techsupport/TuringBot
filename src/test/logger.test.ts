6e35b93bc6576e006202c61bee042d4143c63faa
zleyyij <zleyyij@users.noreply.github.com>
2023-07-29 21:06:34 -0600

    tests: basic logger tests

--- src/test/logger.test.ts
+++ src/test/logger.test.ts
@@ -24,4 +24,40 @@ describe('testing logging', () => {
       timestamp: undefined,
     });
   });
+
+  it('should generate a warning event embed', () => {
+    const generationResult = generateEventEmbed(
+      EventCategory.Warning,
+      'a',
+      'a'
+    );
+    // the timestamp can't be compared, because it can change between the function call
+    // and the assertion, so we just verify that it's a valid date
+    Date.parse(generationResult.timestamp!);
+    generationResult.timestamp = undefined;
+
+    assert.deepStrictEqual(generationResult, {
+      title: 'Event Type: Warning',
+      description: 'Location: a',
+      color: 0xf5f543,
+      fields: [{name: 'Description', value: 'a'}],
+      timestamp: undefined,
+    });
+  });
+
+  it('should generate an error event embed', () => {
+    const generationResult = generateEventEmbed(EventCategory.Error, 'a', 'a');
+    // the timestamp can't be compared, because it can change between the function call
+    // and the assertion, so we just verify that it's a valid date
+    Date.parse(generationResult.timestamp!);
+    generationResult.timestamp = undefined;
+
+    assert.deepStrictEqual(generationResult, {
+      title: 'Event Type: Error',
+      description: 'Location: a',
+      color: 0xd74e2e,
+      fields: [{name: 'Description', value: 'a'}],
+      timestamp: undefined,
+    });
+  });
 });
--- src/test/main.test.ts
+++
@@ -1,5 +0,0 @@
-//import {describe, it} from 'node:test';
-// commented out to prevent npm fix from complaining
-
-// dynamic import statements are used to avoid side affects
-// this is all broken
